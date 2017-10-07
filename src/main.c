fb8bf55e37fe549a61783d29ddfcb8aa63215c13
isakim <bellakm10@gmail.com>
2017-10-07 00:41:49 -0300

    antes tarde do que nunca rsrs

--- src/main.c
+++ src/main.c
@@ -2,13 +2,42 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, i, tarefa;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
+  for(i=1; i<(n+1); i++){
 
+    tarefa=0;
+   
+    if((i%a[0])==0){
+	printf("0");
+	tarefa=1;
+    }
+    if((i%a[1])==0){
+        printf("1");
+	tarefa=1;
+    }
+    if((i%a[2])==0){
+        printf("2");
+	tarefa=1;
+    }
+    if((i%a[3])==0){
+        printf("3");
+	tarefa=1;    
+    }
+    if((i%a[4])==0){
+        printf("4");
+	tarefa=1;
+    }
+
+    if(!tarefa) {
+	printf("-");
+    }
+
+    
+    printf("\n");
+}
+  
   return 0;
 }
